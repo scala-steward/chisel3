fb5f3f03b304b80a31df44d7fc77ebbf43d29815
scala-steward <scala-steward@users.noreply.github.com>
2025-11-26 17:36:58 +0000

    deploy: 40942a3979fd4d0757f89d42fea55fdda7425377

--- releases/snapshot/download/chisel-example.scala
+++ releases/snapshot/download/chisel-example.scala
@@ -1,7 +1,7 @@
 //> using repository https://central.sonatype.com/repository/maven-snapshots
-//> using scala 2.13.17
-//> using dep org.chipsalliance::chisel:2019-03-11-SNAPSHOT+5615-f8e5a7ac-SNAPSHOT
-//> using plugin org.chipsalliance:::chisel-plugin:2019-03-11-SNAPSHOT+5615-f8e5a7ac-SNAPSHOT
+//> using scala 2.13.18
+//> using dep org.chipsalliance::chisel:2019-03-11-SNAPSHOT+5623-40942a39-SNAPSHOT
+//> using plugin org.chipsalliance:::chisel-plugin:2019-03-11-SNAPSHOT+5623-40942a39-SNAPSHOT
 //> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit
 //> using options -Xfatal-warnings -Ywarn-dead-code -Ywarn-unused -Ymacro-annotations
 
