e0591f4c248fb4de93bb5096757c9f72a918f71a
Jack Koenig <koenig@sifive.com>
2026-04-17 11:16:50 -0700

    Rename all object interface traits, $ => Obj (#5274)
    This works around a bug in IntelliJ which assumes that $ signifies an
    inner class and thus it does not properly find public APIs defined in
    these traits.
    
    Make BitPat$Intf change binary compatible.

--- src/main/scala-3/chisel3/util/BitPatIntf.scala
+++ src/main/scala-3/chisel3/util/BitPatIntf.scala
@@ -7,12 +7,14 @@ import chisel3.experimental.SourceInfo
 import scala.collection.mutable
 import scala.util.hashing.MurmurHash3
 
-private[chisel3] trait BitPat$Intf { self: BitPat.type =>
+private[chisel3] trait BitPatObjIntf { self: BitPat.type =>
   implicit class fromUIntToBitPatComparable(x: UInt) {
     def ===(that: BitPat)(using SourceInfo): Bool = that === x
     def =/=(that: BitPat)(using SourceInfo): Bool = that =/= x
   }
 }
+// Polyfill for a Scala 2 binary compatibility workaround
+private[chisel3] trait BitPat$Intf
 
 private[chisel3] trait BitPatIntf extends SourceInfoDoc { self: BitPat =>
   import chisel3.util.experimental.BitSet
